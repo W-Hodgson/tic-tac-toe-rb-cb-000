86fd6c413f757a275bec8d14705e8ab070f6b26a
Will Hodgson <will.hodgson@current-rms.com>
2019-07-23 09:33:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -79,7 +79,11 @@ def turn(board)
 end
 
 def turn_count(board)
+  count = 0
   board.each do |position|
-    
+    if position == "X" || position == "O"
+      count += 1
+    end
   end
+  count
 end
